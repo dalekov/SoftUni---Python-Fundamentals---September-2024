bac92b1ecc0ce7bed300575199662ef6ba813610
deniGnn <166005883+dalekov@users.noreply.github.com>
2024-11-08 13:56:02 +0100

    Create 05_html.py

---
+++ text_processing/more_exercises/05_html.py
@@ -0,0 +1,23 @@
+title = input()
+content = input()
+
+comments = []
+while True:
+    comment = input()
+
+    if comment == 'end of comments':
+        print('<h1>')
+        print(f'    {title}')
+        print('</h1>')
+
+        print('<article>')
+        print(f'    {content}')
+        print('</article>')
+
+        for comment in comments:
+            print('<div>')
+            print(f'    {comment}')
+            print('</div>')
+        break
+
+    comments.append(comment)
