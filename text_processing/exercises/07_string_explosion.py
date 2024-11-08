b14a115b5eed38c1b4949bc203978ce086b1ea41
deniGnn <166005883+dalekov@users.noreply.github.com>
2024-11-08 08:45:31 +0100

    Update 07_string_explosion.py

--- text_processing/exercises/07_string_explosion.py
+++ text_processing/exercises/07_string_explosion.py
@@ -1,14 +1,18 @@
-string = input()
-power = 0
-result = []
+def process_string(s):
+    result = []
+    power = 0
 
-for i in range(len(string)):
-    if string[i] == '>' and i + 1 < len(string):
-        power = int(string[i + 1])
-        result.append(string[i])
-    elif power > 0:
-        power -= 1
-    else:
-        result.append(string[i])
+    for i in range(len(s)):
+        if s[i] == '>':
+            power += int(s[i + 1])
+            result.append(s[i])
+        elif power > 0:
+            power -= 1
+        else:
+            result.append(s[i])
 
-print(''.join(result))
+    return ''.join(result)
+
+s = input()
+
+print(process_string(s))
